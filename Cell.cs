adfbe1aa792e00610c9842e48ed4560e8bb7685c
Alexandre Andrieux <alex@icosacid.com>
2020-03-21 11:16:06 +0100

    Move Cell class to separate file

---
+++ Cell.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Cell {
+	private int status;
+	private int xNbr,yNbr,zNbr;
+	
+	//Accesseurs et mutateurs
+	public Cell(int xNbr, int yNbr, int zNbr, float cellLength) {
+		this.xNbr = xNbr;
+		this.yNbr = yNbr;
+		this.zNbr = zNbr;
+		this.position = Vector3.right*(xNbr*cellLength + cellLength/2f) + Vector3.up*(yNbr*cellLength + cellLength/2) + Vector3.forward*(zNbr*cellLength + cellLength/2);
+	}
+	
+	public int getStatus(){
+		return status;
+	}
+	
+	public void changeStatus(int new_status){
+		this.status = new_status;
+	}
+}
